8a886b547206215f93207e5c11fa90cd80a904f1
Aditya J Parida <126658487+adigithu@users.noreply.github.com>
2025-06-14 11:48:24 +0530

    Add files via upload
    Factorial of a number using iteration

---
+++ Factorial_Iterative.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+unsigned int factorial(unsigned int n){
+    int res=1,i;
+    for(i=2;i<=n;i++){
+        res=res*i;
+    }
+    return res;
+}
+int main(){
+    int num;
+    cout<<"Enter a number:";
+    cin>>num;
+    cout<<"Factorial of "<<num<<" is "<<factorial(num)<<endl;
+    return 0;
+}
